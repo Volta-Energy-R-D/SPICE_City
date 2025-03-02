34e1ebafce7f1995b54ff1ff54e97d253629c6db
takiszourntos <takis.zourntos@emads.org>
2025-03-01 23:33:50 -0500

    adding SC_gridWISE.c and SC_gridWISE.h files...

---
+++ Code/my_includes/SC_gridWISE.h
@@ -0,0 +1,14 @@
+/************************************************** 
+
+SC_gridWISE code
+
+software to enable implementation of:
+
+1) gridWISE DERs/microgrids (powered by dedicated external power)
+
+2) High-Demand Intermittent Loads (attached to the main power grid)
+
+
+
+****************************************************/
+
