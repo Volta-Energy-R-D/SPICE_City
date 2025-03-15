2b34edad35e3eb7b3f77012c230e240f1ca012de
Takis Zourntos <takis.zourntos@emads.org>
2025-03-14 22:14:21 -0400

    updated gridWISE code

--- Code/source/SC_gridWISE.c
+++ Code/source/SC_gridWISE.c
@@ -31,6 +31,13 @@ software to enable implementation of:
 #define     Load5V_2_PB_CB_chan     9
 
 
+//Declare your Phidget channels and other variables
+static  PhidgetDigitalOutputHandle gW_1;
+static  PhidgetDigitalOutputHandle gW_2;
+static  PhidgetDigitalOutputHandle Load5V_1;
+static  PhidgetDigitalOutputHandle Load5V_2;
+
+
 //Declare any event handlers here. These will be called every time the associated event occurs.
 
 static void CCONV onAttach(PhidgetHandle ch, void * ctx) {
@@ -49,66 +56,61 @@ static void CCONV onDetach(PhidgetHandle ch, void * ctx) {
     printf("Detach [%d]!", channel);
 }
 
-int main() {
-    //Declare your Phidget channels and other variables
-    PhidgetDigitalOutputHandle digitalOutput6;
-    PhidgetDigitalOutputHandle digitalOutput7;
-    PhidgetDigitalOutputHandle digitalOutput8;
-    PhidgetDigitalOutputHandle digitalOutput9;
+
 
     //Create your Phidget channels
-    PhidgetDigitalOutput_create(&digitalOutput6);
-    PhidgetDigitalOutput_create(&digitalOutput7);
-    PhidgetDigitalOutput_create(&digitalOutput8);
-    PhidgetDigitalOutput_create(&digitalOutput9);
+    PhidgetDigitalOutput_create(&gW_1);
+    PhidgetDigitalOutput_create(&gW_2);
+    PhidgetDigitalOutput_create(&Load5V_1);
+    PhidgetDigitalOutput_create(&Load5V_2);
 
     //Set addressing parameters to specify which channel to open (if any)
-    Phidget_setHubPort((PhidgetHandle)digitalOutput6, 0);
-    Phidget_setDeviceSerialNumber((PhidgetHandle)digitalOutput6, 767874);
-    Phidget_setChannel((PhidgetHandle)digitalOutput6, 6);
-    Phidget_setHubPort((PhidgetHandle)digitalOutput7, 0);
-    Phidget_setDeviceSerialNumber((PhidgetHandle)digitalOutput7, 767874);
-    Phidget_setChannel((PhidgetHandle)digitalOutput7, 7);
-    Phidget_setHubPort((PhidgetHandle)digitalOutput8, 0);
-    Phidget_setDeviceSerialNumber((PhidgetHandle)digitalOutput8, 767874);
-    Phidget_setChannel((PhidgetHandle)digitalOutput8, 8);
+    Phidget_setHubPort((PhidgetHandle)gW_1, 0);
+    Phidget_setDeviceSerialNumber((PhidgetHandle)gW_1, 767874);
+    Phidget_setChannel((PhidgetHandle)gW_1, 6);
+    Phidget_setHubPort((PhidgetHandle)gW_2, 0);
+    Phidget_setDeviceSerialNumber((PhidgetHandle)gW_2, 767874);
+    Phidget_setChannel((PhidgetHandle)gW_2, 7);
+    Phidget_setHubPort((PhidgetHandle)Load5V_1, 0);
+    Phidget_setDeviceSerialNumber((PhidgetHandle)Load5V_1, 767874);
+    Phidget_setChannel((PhidgetHandle)Load5V_1, 8);
     Phidget_setHubPort((PhidgetHandle)digitalOutput9, 0);
     Phidget_setDeviceSerialNumber((PhidgetHandle)digitalOutput9, 767874);
     Phidget_setChannel((PhidgetHandle)digitalOutput9, 9);
 
     //Assign any event handlers you need before calling open so that no events are missed.
-    Phidget_setOnAttachHandler((PhidgetHandle)digitalOutput6, onAttach, NULL);
-    Phidget_setOnDetachHandler((PhidgetHandle)digitalOutput6, onDetach, NULL);
-    Phidget_setOnAttachHandler((PhidgetHandle)digitalOutput7, onAttach, NULL);
-    Phidget_setOnDetachHandler((PhidgetHandle)digitalOutput7, onDetach, NULL);
-    Phidget_setOnAttachHandler((PhidgetHandle)digitalOutput8, onAttach, NULL);
-    Phidget_setOnDetachHandler((PhidgetHandle)digitalOutput8, onDetach, NULL);
-    Phidget_setOnAttachHandler((PhidgetHandle)digitalOutput9, onAttach, NULL);
-    Phidget_setOnDetachHandler((PhidgetHandle)digitalOutput9, onDetach, NULL);
+    Phidget_setOnAttachHandler((PhidgetHandle)gW_1, onAttach, NULL);
+    Phidget_setOnDetachHandler((PhidgetHandle)gW_1, onDetach, NULL);
+    Phidget_setOnAttachHandler((PhidgetHandle)gW_2, onAttach, NULL);
+    Phidget_setOnDetachHandler((PhidgetHandle)gW_2, onDetach, NULL);
+    Phidget_setOnAttachHandler((PhidgetHandle)Load5V_1, onAttach, NULL);
+    Phidget_setOnDetachHandler((PhidgetHandle)Load5V_1, onDetach, NULL);
+    Phidget_setOnAttachHandler((PhidgetHandle)Load5V_2, onAttach, NULL);
+    Phidget_setOnDetachHandler((PhidgetHandle)Load5V_2, onDetach, NULL);
 
     //Open your Phidgets and wait for attachment
-    Phidget_openWaitForAttachment((PhidgetHandle)digitalOutput6, 5000);
-    Phidget_openWaitForAttachment((PhidgetHandle)digitalOutput7, 5000);
-    Phidget_openWaitForAttachment((PhidgetHandle)digitalOutput8, 5000);
-    Phidget_openWaitForAttachment((PhidgetHandle)digitalOutput9, 5000);
+    Phidget_openWaitForAttachment((PhidgetHandle)gW_1, 5000);
+    Phidget_openWaitForAttachment((PhidgetHandle)gW_2, 5000);
+    Phidget_openWaitForAttachment((PhidgetHandle)Load5V_1, 5000);
+    Phidget_openWaitForAttachment((PhidgetHandle)Load5V_2, 5000);
 
     //Do stuff with your Phidgets here or in your event handlers.
-    PhidgetDigitalOutput_setDutyCycle(digitalOutput6, 1);
-    PhidgetDigitalOutput_setDutyCycle(digitalOutput7, 1);
-    PhidgetDigitalOutput_setDutyCycle(digitalOutput8, 1);
-    PhidgetDigitalOutput_setDutyCycle(digitalOutput9, 1);
+    PhidgetDigitalOutput_setDutyCycle(gW_1, 1);
+    PhidgetDigitalOutput_setDutyCycle(gW_2, 1);
+    PhidgetDigitalOutput_setDutyCycle(Load5V_1, 1);
+    PhidgetDigitalOutput_setDutyCycle(Load5V_2, 1);
 
     //Wait until Enter has been pressed before exiting
     getchar();
 
     //Close your Phidgets once the program is done.
-    Phidget_close((PhidgetHandle)digitalOutput6);
-    Phidget_close((PhidgetHandle)digitalOutput7);
-    Phidget_close((PhidgetHandle)digitalOutput8);
-    Phidget_close((PhidgetHandle)digitalOutput9);
+    Phidget_close((PhidgetHandle)gW_1);
+    Phidget_close((PhidgetHandle)gW_2);
+    Phidget_close((PhidgetHandle)Load5V_1);
+    Phidget_close((PhidgetHandle)Load5V_2);
 
-    PhidgetDigitalOutput_delete(&digitalOutput6);
-    PhidgetDigitalOutput_delete(&digitalOutput7);
-    PhidgetDigitalOutput_delete(&digitalOutput8);
-    PhidgetDigitalOutput_delete(&digitalOutput9);
-}
+    PhidgetDigitalOutput_delete(&gW_1);
+    PhidgetDigitalOutput_delete(&gW_2);
+    PhidgetDigitalOutput_delete(&Load5V_1);
+    PhidgetDigitalOutput_delete(&Load5V_2);
+
